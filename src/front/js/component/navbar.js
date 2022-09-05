53f9feb01464c24f77b8d5c88caa5ffacc580413
Mario <waro.brambilla@gmail.com>
2022-09-05 15:04:46 +0000

    Create Modal

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,33 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import { Modal } from "/workspace/inforMate/src/front/js/component/Modal.js";
 
 //coment test dummie changes
 export const Navbar = () => {
+  const [modalOpen, setModalOpen] = useState (false);
   return (
-<nav class="navbar navbar-expand-lg navbar-dark bg-info sticky-top">
-  <div class="navbar-collapse collapse w-100 order-1 order-md-0 dual-collapse2 ">
+<nav className="navbar navbar-expand-lg navbar-dark bg-info sticky-top">
+  <div className="navbar-collapse collapse w-100 order-1 order-md-0 dual-collapse2 ">
     <Link to="/">
-    <a class="navbar-brand p-3" href="#">InforMATE</a>
+    <a className="navbar-brand p-3" href="#">InforMATE</a>
     </Link>
   </div>
-  <div class="navbar-collapse collapse w-100 order-3 dual-collapse2 d-flex flex-row-reverse" id="navbarTogglerDemo02">
-      <ul class="navbar-nav ml-auto">
-        <li class="nav-item ">
+  <div className="navbar-collapse collapse w-100 order-3 dual-collapse2 d-flex flex-row-reverse" id="navbarTogglerDemo02">
+      <ul className="navbar-nav ml-auto">
+        <li className="nav-item ">
           <Link to="/hotels">          
             <a class="nav-link active" aria-current="page" href="#">Hotels</a>
           </Link>    
         </li>
-        <li class="nav-item ">
+        <li className="nav-item ">
           <Link to="/restaurants">
-            <a class="nav-link active" aria-current="page" href="#">Restaurants</a>
+            <a className="nav-link active" aria-current="page" href="#">Restaurants</a>
           </Link>
         </li>
-        <li class="nav-item ">
+        <li className="nav-item ">
           <Link to="/fights">          
-            <a class="nav-link active" aria-current="page" href="#">Flights</a>
+            <a className="nav-link active" aria-current="page" href="#">Flights</a>
           </Link>
         </li>
-          <button type="button" class=" login btn btn-light mx-3">Login</button>
+          <div className="App">
+          <button className="openModalBtn btn btn-light mx-3 login"onClick={() => {setModalOpen(true);}}> Login </button>
+          {modalOpen && <Modal setOpenModal={setModalOpen} />}
+    </div>
         </ul>
     </div>
 </nav>   
