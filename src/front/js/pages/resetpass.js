b042316c3be9facffab670ccbfe01808684f3712
Mario <waro.brambilla@gmail.com>
2022-09-13 14:20:49 +0000

    Removed Image and refs

--- src/front/js/pages/resetpass.js
+++ src/front/js/pages/resetpass.js
@@ -1,6 +1,5 @@
 import React, { useContext } from "react";
 import { Context } from "../store/appContext";
-import rigoImageUrl from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
 import Card from "react-bootstrap/Card";
 import CardGroup from "react-bootstrap/CardGroup";
