1be4e6bb1775968fe4f8a48727de734e6da1fd75
Sousafer <fernando.ffsousa@gmail.com>
2022-09-23 18:19:18 +0000

    Revert "commit good btn"
    This reverts commit 0f9598b86700918700c5047bba246b5e86accfdb.

--- src/front/js/pages/flights.js
+++ src/front/js/pages/flights.js
@@ -12,16 +12,6 @@ export const Flights = () => {
         <h1 className="pagetitle">
           <span className="navtitle"> Flights </span>
         </h1>
-        <div
-          data-skyscanner-widget="SearchWidget"
-          data-locale="en-GB"
-          data-market="UK"
-          data-currency="EUR"
-        ></div>
-        <script
-          src="https://widgets.skyscanner.net/widget-server/js/loader.js"
-          async
-        ></script>
       </div>
     </div>
   );
